b1fd1fa82104f674bfb42dda8b63b5975784b363
Michał Dyczkowski <michal.dyczkowski@znd.pl>
2024-10-07 00:15:40 +0200

    added recoursive component and basic drag logic

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="app">
-      <a href="https://github.com/David-Desmaisons/draggable-example" target="_blank">
-      <img
-        style="position: fixed; top: 0; right: 0; border: 0; z-index:99999"
-        width="149"
-        height="149"
-        src="https://github.blog/wp-content/uploads/2008/12/forkme_right_gray_6d6d6d.png?resize=149%2C149"
-        class="attachment-full size-full"
-        alt="Fork me on GitHub"
-        data-recalc-dims="1"
-      />
-    </a>
-    <div class="jumbotron logo">
-      <img src="./assets/logo.png">
-    </div>
-    <hello></hello>
-  </div>
+  	<div id="app">
+		<div class="jumbotron logo">
+			<h1>
+				Nested Draggable Test
+			</h1>
+			<img style="margin-top: 3rem;" src="./assets/logo.png">
+		</div>
+    	<TestComponent />
+  	</div>
 </template>
 
 <script>
-import Hello from "./components/Hello";
+import TestComponent from "./components/TestComponent";
 
 export default {
   name: "app",
   components: {
-    Hello
+    TestComponent
   }
 };
 </script>
