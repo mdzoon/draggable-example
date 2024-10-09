67bba6d630ee5097a547db91926cbb17e2364734
Michał Dyczkowski <michal.dyczkowski@znd.pl>
2024-10-09 10:30:00 +0200

    added sorting function to data array

--- src/components/DraggableList.vue
+++ src/components/DraggableList.vue
@@ -53,24 +53,24 @@ export default {
 	data() {
 	  	return {
 			positions: [
-				{ id: 4, title: "Test 4", has_children: false, parent_id: null },
-				{ id: 1, title: "Test 1", has_children: false, parent_id: null },
-				{ id: 2, title: "Test 2", has_children: true, parent_id: null },
-				{ id: 3, title: "Test 3", has_children: true, parent_id: null },
-				{ id: 5, title: "Test 5", has_children: false, parent_id: 2 },
-				{ id: 6, title: "Test 6", has_children: false, parent_id: 2 },
-				{ id: 7, title: "Test 7", has_children: false, parent_id: 2 },
-				{ id: 8, title: "Test 8", has_children: true, parent_id: 2 },
-				{ id: 9, title: "Test 9", has_children: false, parent_id: 3 },
-				{ id: 10, title: "Test 10", has_children: false, parent_id: 3 },
-				{ id: 11, title: "Test 11", has_children: false, parent_id: 3 },
-				{ id: 12, title: "Test 12", has_children: false, parent_id: 3 },
-				{ id: 13, title: "Test 13", has_children: true, parent_id: 8 },
-				{ id: 14, title: "Test 14", has_children: false, parent_id: 13 },
-				{ id: 15, title: "Test 15", has_children: false, parent_id: 13 },
-				{ id: 16, title: "Test 16", has_children: false, parent_id: 15 },
-				{ id: 17, title: "Test 17", has_children: false, parent_id: 16 },
-				{ id: 18, title: "Test 18", has_children: false, parent_id: 17 },
+				{ id: 10, title: "Test 10", has_children: false, parent_id: 3, index: '3b' },
+				{ id: 12, title: "Test 12", has_children: false, parent_id: 3, index: '3d' },
+				{ id: 9, title: "Test 9", has_children: false, parent_id: 3, index: '3a' },
+				{ id: 11, title: "Test 11", has_children: false, parent_id: 3, index: '3c' },
+				{ id: 5, title: "Test 5", has_children: false, parent_id: 2, index: '2a' },
+				{ id: 6, title: "Test 6", has_children: false, parent_id: 2, index: '2b' },
+				{ id: 7, title: "Test 7", has_children: false, parent_id: 2, index: '2c' },
+				{ id: 8, title: "Test 8", has_children: true, parent_id: 2, index: '2d' },
+				{ id: 1, title: "Test 1", has_children: false, parent_id: null, index: '3' },
+				{ id: 2, title: "Test 2", has_children: true, parent_id: null, index: '2' },
+				{ id: 3, title: "Test 3", has_children: true, parent_id: null, index: '4' },
+				{ id: 13, title: "Test 13", has_children: true, parent_id: 8, index: '2da' },
+				{ id: 14, title: "Test 14", has_children: false, parent_id: 13, index: '2daa' },
+				{ id: 15, title: "Test 15", has_children: false, parent_id: 13, index: '2dab' },
+				{ id: 4, title: "Test 4", has_children: false, parent_id: null, index: '1' },
+				{ id: 16, title: "Test 16", has_children: false, parent_id: 15, index: '2daba' },
+				{ id: 17, title: "Test 17", has_children: false, parent_id: 16, index: '2dabaa' },
+				{ id: 18, title: "Test 18", has_children: false, parent_id: 17, index: '2dabaaa' },
 			],
 			originalOrder: [],
 			editable: true,
@@ -79,9 +79,8 @@ export default {
 	  	}
 	},
 	created() {
-		this.positions.forEach((position, index) => {
-			position.originalOrder = index
-		})
+		this.sortInitialPositions()
+		this.assignOrder()
 	},	
 	watch: {
 		isDragging(newValue) {
@@ -107,6 +106,34 @@ export default {
 	  	},
 	},	
 	methods: {
+		sortInitialPositions() {
+			let sortedPositions = []
+			const addChildren = (parentId) => {
+				const children = this.positions.filter(p => p.parent_id === parentId)
+				children.sort((a, b) => a.index.localeCompare(b.index))
+
+				children.forEach(child => {
+					sortedPositions.push(child)
+					addChildren(child.id)
+				})
+			}
+			const topLevelPositions = this.positions.filter(position => position.parent_id === null)
+			topLevelPositions.sort((a, b) => a.index.localeCompare(b.index))
+			topLevelPositions.forEach(parent => {
+				sortedPositions.push(parent)
+			})
+
+			topLevelPositions.forEach(parent => {
+				addChildren(parent.id)
+			})
+
+			this.positions = sortedPositions
+		},
+		assignOrder() {
+			this.positions.forEach((position, index) => {
+				position.originalOrder = index
+			})
+		},		
 		orderPositions() {
 			this.positions.sort((one, two) => one.originalOrder - two.originalOrder)
 		},
